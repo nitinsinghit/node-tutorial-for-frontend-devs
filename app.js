d19158a6cac8d203f8d7095de2e595685f77b851
nitinsingh <nitin_singh@saa.senate.gov>
2018-06-25 09:59:35 -0400

    add env variable for service

--- app.js
+++ app.js
@@ -5,13 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// New Code
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -27,15 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
+
 
 app.use('/', routes);
-app.use('/users', users);
-
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+
+const TESTSAMPLEAPI_SERVICE_HOST = process.env.TESTSAMPLEAPI_SERVICE_HOST;
+const TESTSAMPLEAPI_SERVICE_PORT = process.env.TESTSAMPLEAPI_SERVICE_PORT;
+
+// const TESTSAMPLEAPI_SERVICE_HOST = "testsampleapi-voting-application.paaslab.senate.gov";
+// const TESTSAMPLEAPI_SERVICE_PORT = 8080;
 
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index', { title: 'Express' });
+
+
+    const url =
+        "http://"+TESTSAMPLEAPI_SERVICE_HOST+":"+TESTSAMPLEAPI_SERVICE_PORT;
+    request.get(url, (error, response, body) => {
+        let json = JSON.parse(body);
+        console.log(json);
+    });
+
+
+    
 });
 
 /* GET Hello World page. */
@@ -11,51 +29,6 @@ router.get('/helloworld', function(req, res) {
 	res.render('helloworld', { title: 'Hello, World!' })
 });
 
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
-    });
-});
 
-/* GET New User page. */
-router.get('/newuser', function(req, res) {
-    res.render('newuser', { title: 'Add New User' });
-});
-
-/* POST to Add User Service */
-router.post('/adduser', function(req, res) {
-
-    // Set our internal DB variable
-    var db = req.db;
-
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-
-    // Set our collection
-    var collection = db.get('usercollection');
-
-    // Submit to the DB
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            //res.location("userlist");
-            // And forward to success page
-            res.redirect("userlist");
-        }
-    });
-});
 
 module.exports = router;
